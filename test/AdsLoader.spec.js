d42b08d3e525485d4b8b0ab3999daf1e17addc19
Carlos Serrano <carlos.pastor@mailonline.co.uk>
2016-09-28 21:46:12 +0100

    refactor: rename adsLoader file

---
+++ test/AdsLoader.spec.js
@@ -0,0 +1,11 @@
+"use strict";
+
+import { assert } from 'chai';
+import adsLoader from '../src/adsLoader';
+
+describe('adsLoader', () => {
+  it('must exist', () => {
+    assert.isObject(adsLoader);
+  });
+});
+    
