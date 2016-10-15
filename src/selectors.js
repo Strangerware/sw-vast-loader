09ddb784a1f9d55467a5f5e728146278d93e7eb7
Carlos <carpasse@gmail.com>
2016-10-16 01:49:02 +0200

    feat: add selector methods
    Add method to get an Ad, check if the vast has and ad and to normalise
    the returned vast waterfall

--- src/selectors.js
+++ src/selectors.js
@@ -2,4 +2,25 @@ import get from 'lodash.get';
 
 export const isVastWrapper = (vastObj = {}) => !!get(vastObj, 'vast.ad.wrapper');
 
-export const getVastTagUri = (vastObj = {}) => get(vastObj, 'vast.ad.wrapper.vasttaguri');
+export const getVastTagUri = (vastObj = {}) => get(vastObj, 'vast.ad.wrapper.vastadtaguri');
+
+export const getVastAd = (vastObj = {}) => get(vastObj, 'vast.ad');
+
+export const hasVastAd = (vastObj = {}) => !!getVastAd(vastObj);
+
+export const normaliseWaterfall = (vastObj = {}) => {
+  const ads = getVastAd(vastObj);
+
+  if (Array.isArray(ads)) {
+    const vast = { ...vastObj.vast, ...{ ad: null } };
+    const sharedVastJson = JSON.stringify({ ...vastObj, vast });
+
+    return ads.map((ad) => {
+      const newVastObj = JSON.parse(sharedVastJson);
+      newVastObj.vast.ad = ad;
+      return newVastObj;
+    });
+  }
+
+  return [vastObj];
+};
--- test/selectors.spec.js
+++ test/selectors.spec.js
@@ -1,11 +1,18 @@
 import test from 'ava';
-import { isVastWrapper, getVastTagUri } from '../src/selectors';
+import {
+  isVastWrapper,
+  getVastTagUri,
+  hasVastAd,
+  getVastAd,
+  normaliseWaterfall,
+} from '../src/selectors';
+import { createAsJs } from './fixtures/vastFactory';
 
 const vastWrapperObj = {
   vast: {
     ad: {
       wrapper: {
-        vasttaguri: 'http://expample.com',
+        vastadtaguri: 'http://expample.com',
       },
     },
   },
@@ -32,7 +39,8 @@ test('isVastWrapper must return false otherwise', (t) => {
 });
 
 test('getVastTagUri must return the vastTagUri from the wrapper vastObj', t =>
-  t.is(getVastTagUri(vastWrapperObj), 'http://expample.com'));
+  t.is(getVastTagUri(vastWrapperObj), 'http://expample.com')
+);
 
 test('getVastTagUri must return undefined if vastTagUri can not be found', (t) => {
   t.is(getVastTagUri(nonVastWrapperObj), undefined);
@@ -40,4 +48,43 @@ test('getVastTagUri must return undefined if vastTagUri can not be found', (t) =
   t.is(getVastTagUri(null), undefined);
   t.is(getVastTagUri('nonVastWrapperObj'), undefined);
   t.is(getVastTagUri(() => {}), undefined);
+});
+
+test('getVastAd must return the vast ad or undefined otherwise', (t) => {
+  const ad = {};
+  t.is(getVastAd({ vast: { ad } }), ad);
+  t.is(getVastAd({ vast: { ad: undefined } }), undefined);
+  t.is(getVastAd({}), undefined);
+  t.is(getVastAd(null), undefined);
+  t.is(getVastAd(), undefined);
+});
+
+test('hasVastAd must return true if there is at least an ad on the vastObj and false otherwise', (t) => {
+  t.true(hasVastAd({ vast: { ad: {} } }));
+  t.true(hasVastAd({ vast: { ad: [] } }));
+  t.false(hasVastAd({ vast: { ad: undefined } }));
+  t.false(hasVastAd({}));
+  t.false(hasVastAd(null));
+});
+
+test('normalise waterfall must return an array with the normalised waterfall', async(t) => {
+  const noAdsWaterfall = await createAsJs([]);
+  const oneAdWaterfall = await createAsJs([{ type: 'ad' }]);
+  const vastWrapper = await createAsJs([{ type: 'wrapper' }]);
+  const vastNonWrapper = await createAsJs([{ type: 'ad' }]);
+  const adsWaterfall = await createAsJs([
+    { type: 'wrapper' },
+    { type: 'ad' },
+  ]);
+
+  /* eslint no-param-reassign: ["error", { "props": false }]*/
+  const expected = [vastWrapper, vastNonWrapper]
+    .map((vastObj, index) => {
+      vastObj.vast.ad.$.id = `${index}`;
+      return vastObj;
+    });
+
+  t.deepEqual(normaliseWaterfall(noAdsWaterfall), [noAdsWaterfall]);
+  t.deepEqual(normaliseWaterfall(oneAdWaterfall), [oneAdWaterfall]);
+  t.deepEqual(normaliseWaterfall(adsWaterfall), expected);
 });
