95373aaadb96ebfe9d528b23066ef6947f98aa4a
Carlos <carpasse@gmail.com>
2016-10-16 01:56:07 +0200

    feat: curry waterfall method

--- src/waterfall.js
+++ src/waterfall.js
@@ -1,3 +1,5 @@
+import curry from 'lodash.curry';
+
 const identity = ad => ad;
 
 const doWaterfall = (currentChain, nextChain) =>
@@ -11,7 +13,7 @@ const doWaterfall = (currentChain, nextChain) =>
 
 const error2Array = e => Promise.reject([e]);
 
-export default (wrapperChain, { validate = identity }, ads = []) => {
+export default curry((wrapperChain, { validate = identity }, ads = []) => {
   const createChain = ad => () =>
     wrapperChain(ad)
       .then(validate)
@@ -20,4 +22,4 @@ export default (wrapperChain, { validate = identity }, ads = []) => {
   return ads
     .map(createChain)
     .reduce(doWaterfall, Promise.reject([]));
-};
+}, 3);
