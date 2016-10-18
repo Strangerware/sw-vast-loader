1d0a561b9bc81a4e6324c0ee8cfe19ed7b59aee4
Carlos <carpasse@gmail.com>
2016-10-18 20:04:26 +0100

    test: test tricky xml uri parsing

--- test/vastxml2js.spec.js
+++ test/vastxml2js.spec.js
@@ -30,3 +30,18 @@ test('must be able to deserialise a full vast response', async (t) => {
   const result = await vastxml2js(vast);
   t.deepEqual(result, parsedVast);
 });
+
+test('must be able to deserialise tricky URIs', async (t) => {
+  const trickyXML = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <VAST xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="vast.xsd" version="3.0">
+    <Ad>
+    <Impression id="myserver"> 
+        <![CDATA[http://ad.server.com/impression/dot.gif?s=x]]]]><![CDATA[>]]>x
+    </Impression>
+    </Ad>
+    </VAST>
+  `;
+  const result = await vastxml2js(trickyXML);
+  t.is(result.vast.ad.impression._, 'http://ad.server.com/impression/dot.gif?s=x]]>x');
+});
